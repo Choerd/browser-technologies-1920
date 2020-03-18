f348a0e9f962c397f8696d8951db75e290d9a983
Sjors <sjorspieter@live.nl>
2020-03-18 15:14:24 +0100

    html code

--- opdracht.2/server.js
+++ opdracht.2/server.js
@@ -1,4 +1,3 @@
-require('dotenv').config()
 const express = require('express')
 const port = process.env.PORT || 4000
 const app = express()
@@ -12,5 +11,12 @@ app.set('views', 'templates');
 
 // Routes
 app.get('/', (req, res) => res.render('home'))
+app.get('/generate-user-code', (req, res) => res.render('generate-user-code'))
+app.get('/use-user-code', (req, res) => res.render('use-user-code'))
+app.get('/about-you', (req, res) => res.render('about-you'))
+app.get('/personal', (req, res) => res.render('personal'))
+app.get('/nutrition', (req, res) => res.render('nutrition'))
+app.get('/money', (req, res) => res.render('money'))
+app.get('/conclusion', (req, res) => res.render('conclusion'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
