76ff567d0bb975c41633f633a782f7885ddc5437
Sjors <sjorspieter@live.nl>
2020-03-19 09:20:48 +0100

    added saving to json

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const fs = require('file-system')
 const port = process.env.PORT || 4000
 const app = express()
+
+const urlencodedParser = bodyParser.urlencoded({ extended: true })
 
 // Static assets folder
 app.use(express.static('static'))
@@ -14,7 +18,24 @@ app.get('/', (req, res) => res.render('home'))
 app.get('/generate-user-code', (req, res) => res.render('generate-user-code'))
 app.get('/use-user-code', (req, res) => res.render('use-user-code'))
 app.get('/about-you', (req, res) => res.render('about-you'))
+
 app.get('/personal', (req, res) => res.render('personal'))
+app.post('/personal', urlencodedParser, (req, res) => {
+
+    // formdata = req.body
+    const data = JSON.stringify(req.body, null, 2)
+    fs.writeFileSync('users/data.json', data)
+
+    const read = fs.readFileSync('users/data.json')
+    const data2 = JSON.parse(read)
+    console.log(data2)
+
+
+    res.render('personal')
+})
+
+// (req, res) => res.render('personal'))
+
 app.get('/nutrition', (req, res) => res.render('nutrition'))
 app.get('/money', (req, res) => res.render('money'))
 app.get('/conclusion', (req, res) => res.render('conclusion'))
