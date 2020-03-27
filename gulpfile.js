f7d691fe66edc928db6314be153cafd6e1713816
Sjors <sjorspieter@live.nl>
2020-03-27 12:46:00 +0100

    autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,31 @@ const
     gulp = require('gulp'),
     cssimport = require("gulp-cssimport"),
     babel = require('gulp-babel'),
-    concat = require('gulp-concat')
+    concat = require('gulp-concat'),
+    autoprefixer = require('gulp-autoprefixer')
+
+var AUTOPREFIXER = [
+    '> 1%',
+    'ie >= 8',
+    'edge >= 15',
+    'ie_mob >= 10',
+    'ff >= 45',
+    'chrome >= 45',
+    'safari >= 7',
+    'opera >= 23',
+    'ios >= 7',
+    'android >= 4',
+    'bb >= 10'
+]
+
 
 gulp.task('build:css', () => {
     return gulp.src(['development/css/index.css'])
         .pipe(cssimport({ matchPattern: "*.css" }))
+        .pipe(autoprefixer({
+            browsers: AUTOPREFIXER,
+            cascade: false
+        }))
         .pipe(gulp.dest('production/css'))
 })
 
